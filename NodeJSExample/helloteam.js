4436e123ae0097e51dac9c383217ffe1c78abd9c
Luke Wells <lukewells768@gmail.com>
2023-07-04 18:23:36 -0600

    updated comments

--- NodeJSExample/helloteam.js
+++ NodeJSExample/helloteam.js
@@ -1,5 +1,6 @@
 let teamNames = ["Luke", "Another Luke", "Luke the third"];
 //comment to pull back to local repo for testing.
+//and pushing this comment back to remote repo
 for (const name of teamNames) {
     console.log(name);
 }
